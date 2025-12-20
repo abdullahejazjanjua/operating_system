8baf68a2f65513974954d42e5857bbaa91a81da0
abdullahejazjanjua <abdullahejaz822@gmail.com>
2025-12-21 00:18:23 +0500

    added practice for lab9

--- lab/lab9/task2_1_array_find.c
+++ lab/lab9/task2_1_array_find.c
@@ -31,13 +31,26 @@
 
 #define N 1000000000
 #define MODULO 100000000
+#define NTHREADS 10
+
+typedef struct arg_t 
+{
+	int *arr;
+	long size;
+	int val;
+} arg_t;
+
+typedef struct ret_t
+{
+	int idx;
+} ret_t;
 
 int * make_array_random(long size) 
 {
     srand(time(NULL));
 
-    int * arr = malloc(size*sizeof(long));
-    if (arr==NULL) exit(-1);
+    int *arr = malloc(size * sizeof(long));
+    if (arr == NULL) exit(-1);
 
     for(long i=0; i<size; i++)
         arr[i] = rand()%MODULO;
@@ -49,26 +62,39 @@ int * make_array_random(long size)
 int find_array(int *arr, long size, int val) 
 {
     int idx = -1;
-    for(long i=0; i<size; i++)
-      if (arr[i]==val)
+    for(long i = 0; i < size; i++)
+      if (arr[i] == val)
         idx = i;
 
     return idx;
 }
 
 
-void *find_array_thread(void *arg) 
+void *find_array_thread(void *args) 
 {
 
     /*
      * your implementation goes here.
      * create any helper functions you might need.
      */
+     
+    arg_t arg = *(arg_t*) args;
+    ret_t *ret = (ret_t*) malloc(sizeof(ret_t));
+    ret->idx = -1;
+    
+    for(long i = 0; i < arg.size; i++)
+    {
+    	if(arg.arr[i] == arg.val)
+     	{
+      		ret->idx = i;
+      	}
+    }
 
-    return NULL;
+    return (void*) ret;
 }
 
-int main() {
+int main() 
+{
 
     int * arr = make_array_random(N);
 
@@ -79,14 +105,35 @@ int main() {
     printf("find_array found val=%d at index=%d\n", val, idx1);
 
 
-    int idx2 = -1;
-
+    int idx2 = -1, size_per_thread = N / NTHREADS;
+    arg_t args[NTHREADS];
+    pthread_t tids[NTHREADS];
     /*
      * YOUR IMPLEMANTATION GOES HERE.
      * ADD ANY HELPER FUNCTIONS NEEDED.
-     */
-
+    */
+    
+    for(int i = 0; i < NTHREADS; i++)
+    {
+    	args[i].size = size_per_thread;
+     	args[i].arr = arr + (i * size_per_thread);
+      	args[i].val = val;
+    	pthread_create(&tids[i], NULL, find_array_thread, (void*) &args[i]);
+    }
+    
+    for(int i = 0; i < NTHREADS; i++)
+    {
+    	ret_t *ret;
+     	pthread_join(tids[i], (void **) &ret);
+      	if(ret->idx != -1)
+       	{
+       		idx2 = (i * size_per_thread) + ret->idx; // compute global index
+        }
+        free(ret);
+    }
+     
      printf("using a separate thread found val=%d at index=%d\n", val, idx2);
+     printf("Found: %d\n", arr[idx2]);
 
     free(arr);
     return 0;
